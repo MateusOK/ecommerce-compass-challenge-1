c7d8ebaefc760ab988ddc800b0a0070385b2b30f
MateusOK <mateusribeiro-2004@hotmail.com>
2023-06-12 17:22:16 -0300

    test: mysql connection

--- src/br/com/compass/pb/controller/Stock.java
+++ src/br/com/compass/pb/controller/Stock.java
@@ -1,6 +1,27 @@
 package br.com.compass.pb.controller;
 
+import java.sql.*;
+
 public class Stock {
 
+    private Connection connection;
 
+    public Stock() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "1234");
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to database");
+        }
+    }
+
+    public void addProduct() {
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("insert into products values (1 ,\"soap\", 2.49, 5)");
+            statement.close();
+            System.out.println("Product added successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/br/com/compass/pb/view/Main.java
+++ src/br/com/compass/pb/view/Main.java
@@ -1,17 +1,24 @@
+package br.com.compass.pb.view;
+
 import br.com.compass.pb.controller.Products;
 import br.com.compass.pb.controller.ShoppingCart;
+import br.com.compass.pb.controller.Stock;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        Products prod1 = new Products("AAABBVV", "Detergente", 3.90, 5 );
-        ShoppingCart cart = new ShoppingCart();
+//        Products prod1 = new Products("AAABBVV", "Detergente", 3.90, 5 );
+//        ShoppingCart cart = new ShoppingCart();
+//
+//        cart.addProduct(prod1);
+//
+//        cart.showProducts();
 
-        cart.addProduct(prod1);
+        Stock test = new Stock();
+        test.addProduct();
 
-        cart.showProducts();
 
 
         }
