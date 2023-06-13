16ecf9f634163e12fce6ae5d2d6543cb05bdef18
MateusOK <mateusribeiro-2004@hotmail.com>
2023-06-13 16:07:53 -0300

    feat: get products from database

--- src/br/com/compass/pb/controller/Products.java
+++ src/br/com/compass/pb/controller/Products.java
@@ -1,26 +1,32 @@
 package br.com.compass.pb.controller;
+import br.com.compass.pb.model.Connector;
+
+import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Products {
 
-    public Products(String id, String name, Double price, Integer quantity) {
-        this.id = id;
+    private Integer id;
+    private String name;
+    private double price;
+    private Integer quantity;
+
+    public Products(String name, Double price, Integer quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
 
-    private String id;
-    private String name;
-    private Double price;
-    private Integer quantity;
-
-    public String getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(String id) {
+
+    void setId(Integer id) {
         this.id = id;
     }
+
 
     public String getName() {
         return name;
@@ -30,11 +36,11 @@ public class Products {
         this.name = name;
     }
 
-    public Double getPrice() {
+    public double getPrice() {
         return price;
     }
 
-    public void setPrice(Double price) {
+    public void setPrice(double price) {
         this.price = price;
     }
 
@@ -45,4 +51,34 @@ public class Products {
     public void setQuantity(Integer quantity) {
         this.quantity = quantity;
     }
+
+    public static Set<Products> getAllProducts() {
+        Set<Products> products = new HashSet<>();
+        Connection connection = Connector.getConnection();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM products");
+
+            while (resultSet.next()) {
+                Integer id = resultSet.getInt("id");
+                String name = resultSet.getString("product_name");
+                Double price = resultSet.getDouble("price");
+                Integer quantity = resultSet.getInt("quantity");
+
+                Products product = new Products(name, price, quantity);
+                product.setId(id); // Set the ID obtained from the database
+                products.add(product);
+            }
+
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return products;
+    }
 }
+
+
