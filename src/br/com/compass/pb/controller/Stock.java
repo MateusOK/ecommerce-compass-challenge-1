757d454a1d413ec7b630817fcd98956b4baff7cf
MateusOK <mateusribeiro-2004@hotmail.com>
2023-06-13 16:12:38 -0300

    feat: crud products database

--- src/br/com/compass/pb/controller/Stock.java
+++ src/br/com/compass/pb/controller/Stock.java
@@ -14,14 +14,39 @@ public class Stock {
         }
     }
 
-    public void addProduct() {
+    public void addProduct(Products prod) {
         try {
             Statement statement = connection.createStatement();
-            statement.executeUpdate("insert into products values (1 ,\"soap\", 2.49, 5)");
+            statement.executeUpdate("INSERT INTO products (product_name, price, quantity) VALUES ('" +
+                    prod.getName() + "', " + prod.getPrice() + ", " + prod.getQuantity() + ")");
             statement.close();
             System.out.println("Product added successfully.");
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("There was an error trying to add this product");
         }
     }
+
+    public void removeProduct(int id) {
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("DELETE FROM products WHERE id ='" + id + "'");
+            statement.close();
+            System.out.println("Product removed successfully.");
+        } catch (SQLException e) {
+            System.out.println("There was an error trying to remove this product");
+        }
+    }
+
+    public void updateProduct(Products prod, int id) {
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("UPDATE products SET product_name='" +
+                    prod.getName() + "', price=" + prod.getPrice() + ", quantity=" + prod.getQuantity() + " WHERE id='" + id + "'");
+            statement.close();
+            System.out.println("Product updated successfully.");
+        } catch (SQLException e) {
+            System.out.println("There was an error trying to update this product");
+        }
+    }
+
 }
