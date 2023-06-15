0fd3b2539d8aca283a254afcab46045f7dfd8363
MateusOK <mateusribeiro-2004@hotmail.com>
2023-06-15 16:00:33 -0300

    feat: checkout class

---
+++ src/br/com/compass/pb/controller/Checkout.java
@@ -0,0 +1,28 @@
+package br.com.compass.pb.controller;
+
+public class Checkout {
+
+    Stock stock;
+    ShoppingCart cart;
+
+    public Checkout(ShoppingCart cart, Stock stock) {
+        this.cart = cart;
+        this.stock = stock;
+    }
+
+    public void totalPrice() {
+        cart.showProducts();
+        double totalPrice = 0;
+        for (Products product : cart.getProducts()) {
+            totalPrice += product.getPrice() * product.getQuantity();
+        }
+        System.out.println("Total price of your cart: " + totalPrice + "\n");
+    }
+
+    public void finishCheckout(){
+        for(Products product : cart.getProducts()){
+            stock.updateProductQuantity(product.getId(), product.getQuantity());
+        }
+    }
+
+}
